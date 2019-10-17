c4a90bdee106b28cfd9d0f036b32fcae50a2c4b3
mgmmmmgm <47030912+mgmmmmgm@users.noreply.github.com>
2019-10-17 18:04:55 +0800

    Add files via upload

---
+++ manifest.lua
@@ -0,0 +1,238 @@
+--
+-- A manifest of all the game's assets
+--
+manifest =
+{
+    scripts =
+    {
+        ['Actions.lua'] =
+        {
+            path = "code/Actions.lua"
+        },
+        ['Character.lua'] =
+        {
+            path = "code/Character.lua"
+        },
+        ['CaptionState.lua'] =
+        {
+            path = "code/CaptionState.lua"
+        },
+        ['CaptionStyles.lua'] =
+        {
+            path = "code/CaptionStyles.lua"
+        },
+        ['Dependencies.lua'] =
+        {
+            path = "code/Dependencies.lua"
+        },
+        ['Entity.lua'] =
+        {
+            path = "code/Entity.lua"
+        },
+        ['EntityDefs.lua'] =
+        {
+            path = "code/EntityDefs.lua"
+        },
+        ['ExploreState.lua'] =
+        {
+            path = "code/ExploreState.lua"
+        },
+        ['main.lua'] =
+        {
+            path = "code/main.lua"
+        },
+        ['Map.lua'] =
+        {
+            path = "code/Map.lua"
+        },
+        ['MoveState.lua'] =
+        {
+            path = "code/MoveState.lua"
+        },
+        ['NPCStandState.lua'] =
+        {
+            path = "code/NPCStandState.lua"
+        },
+        ['Panel.lua'] =
+        {
+            path = "code/Panel.lua"
+        },
+        ['PlanStrollState.lua'] =
+        {
+            path = "code/PlanStrollState.lua"
+        },
+        ['ScreenState.lua'] =
+        {
+            path = "code/ScreenState.lua"
+        },
+        ['Selection.lua'] =
+        {
+            path = "code/Selection.lua"
+        },
+        ['StateMachine.lua'] =
+        {
+            path = "code/StateMachine.lua"
+        },
+        ['StateStack.lua'] =
+        {
+            path = "code/StateStack.lua"
+        },
+        ['Storyboard.lua'] =
+        {
+            path = "code/Storyboard.lua"
+        },
+        ['StoryboardEvents.lua'] =
+        {
+            path = "code/StoryboardEvents.lua"
+        },
+        ['Textbox.lua'] =
+        {
+            path = "code/Textbox.lua"
+        },
+        ['Trigger.lua'] =
+        {
+            path = "code/Trigger.lua"
+        },
+        ['Tween.lua'] =
+        {
+            path = "code/Tween.lua"
+        },
+        ['Animation.lua'] =
+        {
+            path = "code/Animation.lua"
+        },
+        ['Util.lua'] =
+        {
+            path = "code/Util.lua"
+        },
+        ['WaitState.lua'] =
+        {
+            path = "code/WaitState.lua"
+        },
+        -- ['World.lua'] =
+        -- {
+        --     path = "code/World.lua"
+        -- },
+        ['MapDB.lua'] =
+        {
+            path = "code/MapDB.lua"
+        },
+        ['FollowPathState.lua'] =
+        {
+            path = "code/FollowPathState.lua"
+        },
+        ['bedroom.lua'] =
+        {
+            path = "map/bedroom.lua"
+        },
+        ['bedroom_night.lua'] =
+        {
+            path = "map/bedroom_night.lua"
+        },
+        ['world.lua'] =
+        {
+            path = "map/world.lua"
+        },
+    },
+    textures =
+    {
+        -- ['rpg_indoor.png'] =
+        -- {
+        --     path = "art/rpg_indoor.png",
+        -- },
+        ['cursor.png'] =
+        {
+            path = "art/cursor.png",
+        },
+        ['gradient_panel.png'] =
+        {
+            path = "art/gradient_panel.png",
+        },
+        ['continue_caret.png'] =
+        {
+            path = "art/continue_caret.png",
+        },
+        -- ['scrollbar.png'] =
+        -- {
+        --     path = "art/scrollbar.png",
+        -- },
+        ['inventory_icons.png'] =
+        {
+            path = "art/inventory_icons.png",
+        },
+        ['atlas.png'] =
+        {
+            path = "art/atlas.png",
+        },
+        ['Frisk_1.png'] =
+        {
+            path = "art/Frisk_1.png",
+        },
+        ['collision_graphic.png'] =
+        {
+            path = "art/collision_graphic.png",
+        },
+        ['world.png'] =
+        {
+            path = "art/world.png",
+        },
+        ['witch.png'] =
+        {
+            path = "art/witch.png",
+        },
+        ['teacher.png'] =
+        {
+            path = "art/teacher.png",
+        },
+        ['friend.png'] =
+        {
+            path = "art/friend.png",
+        },
+        ['billboard.png'] =
+        {
+            path = "art/billboard.png",
+        },
+        ['dog.png'] =
+        {
+            path = "art/dog.png",
+        },
+        ['elf.png'] =
+        {
+            path = "art/elf.png",
+        },
+    },
+    ['fonts'] =
+    {
+        ["default"] =
+        {
+            path = "art/junction.ttf",
+        },
+        ["title"] =
+        {
+            path = "art/adrip1.ttf",
+        },
+        ["body"] =
+        {
+            path = "art/slab.ttf",
+        }
+    },
+    ['sounds'] =
+    {
+        ['wind'] =
+        {
+            path = "sound/wind.wav"
+        },
+        ['thunder'] =
+        {
+            path = "sound/thunder.wav"
+        },
+        ['closet'] =
+        {
+            path = "sound/closet.wav"
+        },
+        ['radionoise'] =
+        {
+            path = "sound/radionoise_original.wav"
+        }
+    }
+}
---
+++ settings.lua
@@ -0,0 +1,11 @@
+
+name = "My Strange Life"
+width = 1280/2
+height = 800/2
+manifest = "manifest.lua"
+main_script = "main.lua"
+on_update = "update()"
+
+webserver = false
+display_width = width*2
+display_height = height*2
